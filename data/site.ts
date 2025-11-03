e7897aebaecfe53aadf038e65c7c087da1515d0e
livinda-dev <sounsok626@gmail.com>
2025-11-03 09:16:50 +0700

    change CV

--- data/site.ts
+++ data/site.ts
@@ -6,6 +6,6 @@ export const siteConfig = {
   email: "sokda106@gmail.com", // TODO: Replace with your email
   github: "https://github.com/livinda-dev", // TODO: Replace with your GitHub
   linkedin: "https://www.linkedin.com/in/chanda-sok-9219622b4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", // TODO: Replace with your LinkedIn
-  resumeUrl: "/Chanda_Sok_CV.pdf", // TODO: Replace with your resume file in public/
+  resumeUrl: "/chanda_sok-resume.pdf", // TODO: Replace with your resume file in public/
     profile:"/projects/profile.png"
 };
